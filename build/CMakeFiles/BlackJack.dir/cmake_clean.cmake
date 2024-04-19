02d3fcdca35d21d07cceebed2d23a7f90f578f2e
Murban6760 <156224924+Murban6760@users.noreply.github.com>
2024-04-19 13:33:10 +0000

    updateed C make

---
+++ build/CMakeFiles/BlackJack.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "BlackJack"
+  "BlackJack.pdb"
+  "CMakeFiles/BlackJack.dir/BlackJack.cpp.o"
+  "CMakeFiles/BlackJack.dir/CardDeck.cpp.o"
+  "CMakeFiles/BlackJack.dir/Dealer.cpp.o"
+  "CMakeFiles/BlackJack.dir/Player.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BlackJack.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
